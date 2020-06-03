dbfc8865ef83fdc8f82e1a6fc3ed30056307595b
James Phillips <james.phillips@cognitran.com>
2020-06-03 18:31:27 +0100

    added checkput to git commands.

--- dot/dot.py
+++ dot/dot.py
@@ -21,7 +21,7 @@ class Dot(object):
         self.cfg._input('repo', 'What\'s your git repository url')
         self.cfg._input('email', 'What\'s your email')
         self.args = sys.argv[1:]
-        self.git_commands = ['add','am','apply','archive','bisect','blame','branch','bundle','cherry','cherry-pick','citool','clean','clone','commit','config','describe','diff','difftool','fetch','format-patch','fsck','gc','gitk','grep','gui','help','init','instaweb','log', 'ls-tree', 'merge','mergetool','mv','notes','pull','push','range-diff','rebase','reflog','remote','repack','replace','request-pull','reset','revert','rm','send-email','shortlog','show','show-branch','stage','stash','status','submodule','tag','whatchanged','worktree',]
+        self.git_commands = ['add','am','apply','archive','bisect','blame','branch','bundle', 'checkout', 'cherry','cherry-pick','citool','clean','clone','commit','config','describe','diff','difftool','fetch','format-patch','fsck','gc','gitk','grep','gui','help','init','instaweb','log', 'ls-tree', 'merge','mergetool','mv','notes','pull','push','range-diff','rebase','reflog','remote','repack','replace','request-pull','reset','revert','rm','send-email','shortlog','show','show-branch','stage','stash','status','submodule','tag','whatchanged','worktree',]
         self.git = sh.git.bake(f"--git-dir={os.path.join(self.cfg.home, '.cfg')}",  f"--work-tree={self.cfg.home}", "-c", f"user.name={self.cfg.username}", "-c", f"user.email={self.cfg.email}")
         self.commands = ['status', 'ls']
         self.check_create_repo()
