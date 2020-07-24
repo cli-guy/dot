94434846cf5a58e9f9d330b044022975b0a558c6
James Phillips <james.phillips@cognitran.com>
2020-07-24 16:25:29 +0100

    fix init

--- dot/dot.py
+++ dot/dot.py
@@ -37,7 +37,7 @@ class Dot(object):
         if not os.path.exists(os.path.join(self.cfg.home, ".cfg")):
             with open(os.path.join(self.cfg.home, '.gitignore'), 'w') as file:
                 file.write(".cfg")
-            clone = call(["git", "clone", "--bare", self.cfg.repo, os.path.join(self.cfg.home, '.cfg')])
+            clone = run(["git", "clone", "--bare", self.cfg.repo, os.path.join(self.cfg.home, '.cfg')])
             if clone.returncode == 0:
                 self.git('config', '--local', 'status.showUntrackedFiles', 'no')
                 self.git.checkout("master", ".")
