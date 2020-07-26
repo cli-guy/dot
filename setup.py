1b314c751fcc998986bc10577467a07b9ed7cda9
James Phillips <james.phillips@cognitran.com>
2020-07-26 22:58:49 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dot',
+    name='jp-dot',
     version='1.0',
     description='dot, a git bare repository wrapper for managing dotfiles.',
     author='James Phillips',
     license='GPLv3',
     packages=find_packages(),
     author_email='james@mightysteedit.co.uk',
-    install_requires=["sh"],
+    install_requires=["jp-config"],
     url="https://github.com/cli-guy/dot",
     entry_points = {
         'console_scripts' : ['dot = dot.dot:main']
