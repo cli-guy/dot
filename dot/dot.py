b5cad1745112f3c7d4b4db636e8adcdd24e3d2e7
James Phillips <james.phillips@cognitran.com>
2020-07-26 09:40:11 +0100

    removed sh module

--- dot/dot.py
+++ dot/dot.py
@@ -13,16 +13,17 @@ from jpconfig import Config
 
 class Dot(object):
     def __init__(self):
+        self.args = sys.argv[1:]
         self.home  = os.environ['HOME']
         self.local = os.path.join(self.home, '.cfg')
         self.old_dotfiles = os.path.join(self.home, ".old_dotfiles")
-        self.cfg   = Config(os.path.join(self.home, '.dot'))
-        self.cfg.username = input('What\'s your full name for git')
-        self.cfg.remote = input('What\'s your git repository url')
-        self.cfg.email = input('What\'s your email')
-        self.args = sys.argv[1:]
+        self.cfg   = Config(file=os.path.join(self.home, '.dot'))
+        if len(self.args) > 0 and self.args[0] == 'init':
+            self.cfg.username = input('What\'s your full name for git')
+            self.cfg.remote = input('What\'s your git repository url')
+            self.cfg.email = input('What\'s your email')
         self.git_commands = ['add','am','apply','archive','bisect','blame','branch','bundle', 'checkout', 'cherry','cherry-pick','citool','clean','clone','commit','config','describe','diff','difftool','fetch','format-patch','fsck','gc','gitk','grep','gui','help','init','instaweb','log', 'ls-tree', 'merge','mergetool','mv','notes','push','range-diff','rebase','reflog','remote','repack','replace','request-pull','reset','revert','rm','send-email','shortlog','show','show-branch','stage','stash','status','submodule','tag','whatchanged','worktree',]
-        self.git_opts = ["git", "--git-dir={self.local}",  f"--work-tree={self.home}", "-c", f"user.name={self.cfg.username}", "-c", f"user.email={self.cfg.email}"]
+        self.git_opts = ["git", f"--git-dir={self.local}",  f"--work-tree={self.home}", "-c", f"user.name={self.cfg.username}", "-c", f"user.email={self.cfg.email}"]
         self.commands = ['ls']
         self.check_create_repo()
         if len(self.args) > 0 and self.args[0] in self.commands:
